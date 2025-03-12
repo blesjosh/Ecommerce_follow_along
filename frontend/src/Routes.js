7a0038af43aedfa011196ff39de27fcd5a8c719e
Blessing Joshua <blessingjoshuaoffl@gmail.com>
2025-03-12 09:43:57 +0530

    Completed

---
+++ frontend/src/Routes.js
@@ -0,0 +1,21 @@
+import LoginPage from "./pages/LoginPage";
+import  SignupPage  from "./pages/SignupPage";
+import Home from "./pages/Home"
+import CreateProduct from "./pages/CreateProduct";
+import MyProducts from "./pages/MyProducts";
+import Cart from "./pages/cart.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx";
+import Profile from "./pages/profile.jsx"
+import CreateAddress from "./pages/CreateAddress.jsx";
+
+export {
+    LoginPage,
+    SignupPage,
+    Home,
+    CreateProduct,
+    MyProducts,
+    Cart,
+    ProductDetails,
+    Profile,
+    CreateAddress
+}
